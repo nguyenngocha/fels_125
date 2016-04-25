d602f725dfcc58b46a53110ab6068527b8ea2db7
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-04-25 15:10:48 +0700

    login-logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   resources :users, only: [:new, :create, :show]
   resources :categories, only: [:index] do
     resources :lessons, only: [:create, :show]
