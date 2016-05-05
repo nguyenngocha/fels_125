ca9a97cc44aaff7f7cb3af1bd64edba1ff97ffe5
nguyenngocha <dodaihoc.abvk@gmail.com>
2016-05-05 03:28:01 +0000

    follow-user

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "bootstrap-will_paginate", "0.0.10"
 gem "config"
 
 group :development, :test do
+  gem "faker", "1.4.2"
   gem "byebug"
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Example User",
+  email: "example@railstutorial.org",
+  password: "foobar",
+  password_confirmation: "foobar",
+  role: 1)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    role: 0)
+end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
